9ab6d9fe261f6c860c2a88bf403da9bf0432712c
MaxMEllon <maxmellon1994@gmail.com>
2018-04-12 21:30:08 +0900

    Refactoring test codes

--- test/map.test.js
+++ test/map.test.js
@@ -1,57 +1,45 @@
 const assert = require('power-assert')
 const { map } = require('../dist/mini-fp')
 
-describe('map()', () => {
-  describe('success', () => {
-    it('Array', () => {
+describe('map() function', () => {
+  describe('success cases', () => {
+    it('expected [1, 2, 3] when apply to Array like a [0, 1, 2] and x => x + 1 as function', () => {
       const mapper = map(x => x + 1)
       assert.deepEqual(mapper([0, 1, 2]), [1, 2, 3])
     })
 
-    it('Set', () => {
+    it('expected [2, 3, 4] when apply to Set{1, 2, 3} and x => x + 1 as function', () => {
       const mapper = map(x => x + 1)
       assert.deepEqual(mapper(new Set([1, 2, 3])), [2, 3, 4])
     })
 
-    it('String', () => {
+    it('expected ["a", "b", "c"] when apply to like a "abc" and x => x  as function', () => {
       const mapper = map(x => x)
       assert.deepEqual(mapper('abc'), ['a', 'b', 'c'])
     })
 
-    it('Object', () => {
+    it('expected [["a", 1], ["b", 2]] when apply to like a { a: 1, b: 2 } and (k, y) => [k, v]  as function', () => {
       const mapper = map((val, key) => [key, val])
-      assert.deepEqual(mapper({ a: 1, b: 2, c: 3 }), [['a', 1], ['b', 2], ['c', 3]])
+      assert.deepEqual(mapper({ a: 1, b: 2 }), [['a', 1], ['b', 2]])
     })
 
-    it('Map', () => {
-      const mapper = map((val, key) => [key, val])
-      const obj = { a: 1, b: 2, c: 3 }
-      const props = Object.keys(obj)
-      const m = new Map(mapper(obj))
-      map((x, index) => {
-        assert.equal(x[0], props[index])
-        assert.equal(x[1], obj[props[index]])
-      })(m)
-    })
-  })
-
-  describe('failure', () => {
-    it('null', () => {
+  describe('failure cases', () => {
+    it('expected thrown TypeError apply to null', () => {
       const mapper = map(x => x)
       assert.throws(() => mapper(null), TypeError)
     })
 
-    it('undefined', () => {
+    it('expected thrown TypeError apply to undefined', () => {
       const mapper = map(x => x)
       assert.throws(() => mapper(void 0), TypeError)
     })
 
-    it('Number', () => {
+    it('expected thrown TypeError apply to Number', () => {
       const mapper = map(x => x)
       assert.throws(() => mapper(1), TypeError)
     })
 
-    it('Symbol', () => {
+    it('expected thrown TypeError apply to Symbol', () => {
       const mapper = map(x => x)
       assert.throws(() => mapper(new Symbol('test')), TypeError)
     })
